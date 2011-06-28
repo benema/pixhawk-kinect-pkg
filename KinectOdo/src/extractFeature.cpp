7732ea860b6a6d246a245c6d9b608bd5e2bfa715
Benedikt Mathis <benema@benema-laptop.(none)>
2011-06-28 12:24:04 +0200

    test

--- KinectOdo/src/extractFeature.cpp
+++ KinectOdo/src/extractFeature.cpp
@@ -3106,7 +3106,7 @@ void EXTRACT::imuCallback (const sensor_msgs::Imu& imuMsg)
 	btMatrix3x3 m(q);
 	double Roll, Pitch, Yaw;
 	m.getRPY(Roll, Pitch, Yaw);
-	m.setRPY(0,-Pitch,-Roll);
+	m.setRPY(-pitch,0,Roll);
 
 	Eigen::Vector3f tmp_vec;
 
